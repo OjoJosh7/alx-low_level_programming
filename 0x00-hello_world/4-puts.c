1689edef0f06662d1e7740b6fdf2b1062f0bbc13
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-11 04:08:12 -0700

    My fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-# include<stdio.h>
-int main()
+# include < stdio.h >
+
+int main(void)
 {
-	char hello[]="Programming is like building a multilingual puzzle";
-		puts(hello);
-		return o;
+	char Hello[] = "Programming is like building a multilingual puzzle";
+	puts(Hello);
+	return (0);
 }
