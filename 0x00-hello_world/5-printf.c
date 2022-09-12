4cfc9839fcec3b59b38a28bd53f1c4959a4a7af5
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-12 05:37:14 -0700

    My sixth commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - it initiates the code block
+ * printf: displays a text
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
