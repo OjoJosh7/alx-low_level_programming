fe343df61fb6d1f6bd848bc16d1c3b596cdb9b5d
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-12 05:00:14 -0700

    My fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 # include<stdio.h>
 
 /**
- *main - displays a sentence
- *
+ *main - print the string in the put function
+ * Description: using the main function
  *Returns: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
