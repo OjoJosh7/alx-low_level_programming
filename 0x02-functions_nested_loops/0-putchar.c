e5369cbcdbf134f77cdb80beba187628cd13f1d8
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-13 10:28:11 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - It initiates the code block 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *ml = "_putchar";
+
+	while (*ml)
+	{
+		_putchar(*ml);
+		ml++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
