c866e14c2de00f06de7479633ef19e8ea7003792
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-12 21:48:17 -0700

    My fourth commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - it initiates the code in the block
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+char y;
+
+for (y = 'a'; y <= 'z'; y++)
+	putchar(y);
+for (y = 'A'; y <= 'Z'; y++)
+	putchar(y);
+putchar('\n')
+	return (0);
+}
