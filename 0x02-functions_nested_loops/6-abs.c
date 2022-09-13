af2b97db4a2911302f32c021c10617ac127d69a6
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-13 14:26:46 -0700

    My seventh commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * _abs - it is a function that computes the absolute value of an integer
+ * @p: this is an inputed integer
  * Return: always 0 (success)
  */
 
-int _abs(int)
+int _abs(int p)
 {
-	return (ab * ((ab > 0) - (ab < 0)));
+	return (p * ((p > 0) - (p < 0)));
 }
