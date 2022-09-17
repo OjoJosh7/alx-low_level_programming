92b5fa2b4c4797af2cd9f5da5526486453c565ef
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-16 19:46:10 -0700

    My fourth commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,5 +10,5 @@ int j;
 
 for (j = 0; j <= 9; j++)
 _putchar(j + '0');
-_putchar ('\n);
+_putchar ('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,11 +4,12 @@
  * followed by a new line, ad does not print 2 and 4
  * Return: result
  */
-void print_most_numbers(void)
-{
-	int i;
 
-	for (i = 0; 1 <= 9; 1++)
+void print_most_numbers(void)
+{	
+int i;
+	
+for (i = 0; 1 <= 9; 1++)
 	{
 		if ((i == 2) || (i == 4))
 			continue;
