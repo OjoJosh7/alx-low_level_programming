1e111f789059924028731daa14c2e82447b52ffb
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-18 15:31:19 -0700

    My second commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,15 @@
  */
 
 void print_most_numbers(void)
-{	
+{
 int i;
-	
+
 for (i = 0; i <= 9; i++)
-	{
-		if ((i == 2) || (i == 4))
-			continue;
-		else
-			_putchar (i + '0');
-	}
-	_putchar('\n');
+{
+if ((i == 2) || (i == 4))
+continue;
+else
+_putchar (i + '0');
+}
+_putchar('\n');
 }
