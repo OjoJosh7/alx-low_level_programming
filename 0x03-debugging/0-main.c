65625284bc817e6001bdda026e5528deb49046be
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-18 15:14:57 -0700

    M second commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Return: always 0 (success)
+ * Return: 0
  */
 
 int main(void)
@@ -11,6 +11,6 @@ int i;
 
 i = 0;
 positive_or_negative(i);
-	
+
 return (0);
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-This folder is about manual debugging
