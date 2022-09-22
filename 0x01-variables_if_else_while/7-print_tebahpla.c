71136b4465915085b0877db856019c34ee3e62e0
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-22 07:10:35 -0700

    My eighth commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char temp;
+
+	for (temp = 'z'; temp >= 'a'; temp--)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
