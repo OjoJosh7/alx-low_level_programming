2136d908def01ec0ba116a6b6b7be95ba61c0a37
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-21 18:51:16 -0700

    My eighth commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - it is a function that encodes a string
+ * @n: string inputed
+ * Return: always 0 (success)
+ */
+
+char *leet(char *n)
+{
+	int i, a;
+	int Alph[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int Num[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (a = 0; a <= 10; a++)
+		{
+			if (n[i] == Alph[a])
+			{
+				n[i] = Num[a / 2];
+				a = 10;
+			}
+		}
+	}
+}
