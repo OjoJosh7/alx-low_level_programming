02cb1d0e27abc9793570370883aee61a909a0517
OjoJosh7 <OjoJosh97@gmail.com>
2022-09-29 15:07:22 -0700

    first

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,7 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	_putchar('\n');
+	if (*s == '\0')
+	{  
+	       _putchar(*s);
+	       _puts_recursion(s + 1);
+	}
+	else
+	{
+	      _putchar('\n');
+	}
 }
