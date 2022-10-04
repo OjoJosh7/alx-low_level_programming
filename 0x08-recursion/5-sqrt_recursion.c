9097ad2c06a25e23d216797c1d0b210ab9714c1e
OjoJosh7 <OjoJosh97@gmail.com>
2022-10-04 02:50:18 -0700

    Sixth

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ *  _num_check - a function that checks if a number has natural square root
+ * @x: inputed number
+ * @y: square of number
+ * Return: square root of number
+ */
+
+int _num_check(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	else if (x * x > y)
+		return (_num_check(y, x - 1));
+	else if (x * x < y)
+		return (_num_check(y, x + 1));
+	else
+		return (-1);
+
+}
+
+/**
  * _sqrt_recursion - a function that returns the natural square root
  * of a number.
  * @n: inputed integer
